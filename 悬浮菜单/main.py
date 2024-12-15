2726e2648ac87cbe0ca1aada80cb45175edd19f7
Xiaoxiaoyu1321 <Xiaoxiaoyu1321@outlook.com>
2024-12-15 12:47:30 +0800

    V2.2.0 Version Update

---
+++ 悬浮菜单/main.py
@@ -0,0 +1,11 @@
+#导入必须的库
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtGui import QFont ,QIcon
+from PyQt5.QtCore import  Qt
+import sys
+import os
+import random
+import time
+import _thread
+
+class MyWindow()
